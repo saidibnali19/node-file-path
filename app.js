fa93d3270c89773938c36f0f00ae699b78588bab
Said Ali <saidibnali19@gmail.com>
2023-08-11 08:29:46 +0300

    feat: used the join and resolve methods of the path module

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ console.log(path.dirname(example));
 console.log(path.basename(example));
 console.log(path.extname(example));
 console.log(path.basename(example, path.extname(example)));
+
+const name = "jane";
+console.log(path.join("/", "users", name, "notes.txt"));
+console.log(path.resolve(example));
